18badacae2460cf438e1346051cf92c5fab815fe
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-02-05 12:11:29 +0600

    cross join

--- routes/web.php
+++ routes/web.php
@@ -280,12 +280,24 @@ Route::get('/', function () {
                 })
                 ->get();*/
 
-        $result = DB::table('rooms')
+        /*$result = DB::table('rooms')
                 ->leftJoin('reservations', 'rooms.id', '=', 'reservations.room_id')
                 ->selectRaw('room_size, price, count(reservations.id) as reservations_count')
                 ->groupBy('room_size', 'price')
                 ->orderByRaw('count(reservations.id) DESC')
+                ->get();*/
+
+        $result = DB::table('rooms')
+                ->crossJoin('cities')
+                ->leftJoin('reservations', function($join){
+                    $join->on('rooms.id', '=', 'reservations.room_id')
+                    ->on('cities.id', '=', 'reservations.city_id');
+                })
+                ->selectRaw('count(reservations.id) as reservations_count, room_size, cities.name')
+                ->groupBy('room_size', 'cities.name')
+                ->orderByRaw('rooms.room_size DESC')
                 ->get();
+
 
         dump($result);
 /*====================================where and first==========================================================================*/
